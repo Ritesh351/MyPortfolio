8eca7dbadacedc7e1795091632d11372357fdf1f
Ritesh Kumar Gupta <67494110+Ritesh351@users.noreply.github.com>
2021-07-26 23:53:38 +0530

    Add Initial Portfolio Website Files

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 150
+        }, 2000, function() {
+            window.location.hash = target;
+        });
+    });
+
+    $('#up').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+});
